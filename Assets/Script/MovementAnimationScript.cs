78cabcb41367f54a7f8ed96da46471cacb5b851a
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-11-04 16:59:36 +0200

    Added zombie movement

---
+++ Assets/Script/MovementAnimationScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovementAnimationScript : MonoBehaviour {
+    public Animator animator;
+    public RuntimeAnimatorController frontController;
+    public RuntimeAnimatorController backController;
+    public RuntimeAnimatorController rightController;
+    public RuntimeAnimatorController leftController;
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(HandleIt());
+    }
+    private IEnumerator HandleIt()
+    {
+        var prevPos = this.transform.position;
+        yield return new WaitForSeconds(0.1f);
+        var actualPos = this.transform.position;
+
+        if (actualPos.x - prevPos.x < 0)
+        {
+            animator.runtimeAnimatorController = leftController;
+        }
+        if (actualPos.x - prevPos.x > 0)
+        {
+            animator.runtimeAnimatorController = rightController;
+        }
+        if (actualPos.x == prevPos.x)
+        {
+            if (actualPos.y - prevPos.y < 0)
+            {
+                animator.runtimeAnimatorController = frontController;
+            }
+            if (actualPos.y - prevPos.y > 0)
+            {
+                animator.runtimeAnimatorController = backController;
+            }
+        }
+        if (actualPos.x == prevPos.x && actualPos.y == prevPos.y)
+        {
+            animator.StartPlayback();
+        }
+        else
+        {
+            animator.StopPlayback();
+        }
+    }
+
+}
--- Assets/Script/RigidMovement.cs
+++ Assets/Script/RigidMovement.cs
@@ -3,11 +3,6 @@
 public class RigidMovement : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
-    public Animator animator;
-    public RuntimeAnimatorController frontController;
-    public RuntimeAnimatorController backController;
-    public RuntimeAnimatorController rightController;
-    public RuntimeAnimatorController leftController;
     public AudioSource audio;
     void Start()
     {
@@ -19,34 +14,13 @@ public class RigidMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        if (moveHorizontal < 0)
-        {
-            animator.runtimeAnimatorController = leftController;
-        }
-        if (moveHorizontal > 0)
-        {
-            animator.runtimeAnimatorController = rightController;
-        }
-        if (moveHorizontal == 0)
-        {
-            if (moveVertical < 0)
-            {
-                animator.runtimeAnimatorController = frontController;
-            }
-            if (moveVertical > 0)
-            {
-                animator.runtimeAnimatorController = backController;
-            }
-        }
         rigidbody.velocity = new Vector3(moveHorizontal * 150, moveVertical * 150, 0);
         if(rigidbody.velocity.x == 0 && rigidbody.velocity.y == 0)
         {
-            animator.StartPlayback();
             audio.FadeOut(2f);
         }
         else
         {
-            animator.StopPlayback();
             audio.volume = 1f;
             audio.mute = false;
         }
