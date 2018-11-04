b4afc1334aedec5fa4a217a7b8b64f7d687d50a3
Tautrimas Gumbys <tautrimas.g2@gmail.com>
2018-11-04 11:01:07 +0200

    Zombie collision with player

---
+++ Assets/Script/ZombieController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+
+    private int currentHealth = 2;
+    private int maxHealth = 2;
+
+    void Start () {
+		
+	}
+    void Update()
+    {
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "PlayerAttack")
+        {
+            currentHealth--;
+        }
+    }
+}
