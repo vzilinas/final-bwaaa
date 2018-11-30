24420a4f717eb8069028eb4efdadbdcb6c7f39e5
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-11-30 13:58:12 +0200

    Started working on graveyard

--- Assets/Script/Exorcist/CameraFollow.cs
+++ Assets/Script/Exorcist/CameraFollow.cs
@@ -9,18 +9,18 @@ public class CameraFollow : MonoBehaviour
     {
         if(player)
         {
-            if (player.position.x > 10 && player.position.y > -62)
-            {
+            // if (player.position.x > 10 && player.position.y > -62)
+            // {
                 transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
-            }
-            else if (player.position.x < 10 && player.position.y > -62)
-            {
-                transform.position = new Vector3(10 + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
-            }
-            else if (player.position.x > 10 && player.position.y < -62)
-            {
-                transform.position = new Vector3(player.position.x + offset.x, offset.y - 62, offset.z); // Camera follows the player with specified offset position
-            }
+            // }
+            // else if (player.position.x < 10 && player.position.y > -62)
+            // {
+            //     transform.position = new Vector3(10 + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+            // }
+            // else if (player.position.x > 10 && player.position.y < -62)
+            // {
+            //     transform.position = new Vector3(player.position.x + offset.x, offset.y - 62, offset.z); // Camera follows the player with specified offset position
+            // }
         }
        
     }
