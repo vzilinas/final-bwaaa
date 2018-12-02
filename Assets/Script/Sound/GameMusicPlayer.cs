36b6c1bb90b2ddced5e34928d4059c02d5a65ca9
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-12-02 17:29:48 +0200

    Added Music to menu

---
+++ Assets/Script/Sound/GameMusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMusicPlayer : MonoBehaviour
+{
+    private static GameMusicPlayer instance = null;
+    public static GameMusicPlayer Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Script/Sound/KillMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(GameObject.Find("Music"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
